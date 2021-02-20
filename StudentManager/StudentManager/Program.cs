9188ccb4c038f893907f16aed25ac3cf0789c327
Denis Kiryanov <denis.kiryanov@arcadia.spb.ru>
2021-02-20 13:42:02 +0300

    Initial commit

---
+++ StudentManager/StudentManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
